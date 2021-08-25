fd7bc9b2d4cced320eea8682574b69ba9e8e83a4
omarshams97 <46492810+omarshams97@users.noreply.github.com>
2021-08-25 13:53:54 -0300

    Update reduce.js

--- tasks/6. Implement reduce/reduce.js
+++ tasks/6. Implement reduce/reduce.js
@@ -1,3 +1,14 @@
+const values = [0,1,2,3,4,5];
 export function Reduce(array, reducer, initialValue) {
-  // TODO: write your code here
+ for(var i = 0; i< array.length; i++){
+    var x = reducer(initialValue,array[i]);
+    initialValue = x;
+ }
+ return initialValue
 }
+
+export function reducer(acc, curr) { 
+  return acc += curr;
+}
+
+console.log(Reduce(values,reducer,0));
