cfa0226a6c3fbf97748b5ec4d76ef2001df91ecd
omarshams97 <46492810+omarshams97@users.noreply.github.com>
2021-08-25 13:24:06 -0300

    Update fizzbuzz.js

--- tasks/4. FizzBuzz/fizzbuzz.js
+++ tasks/4. FizzBuzz/fizzbuzz.js
@@ -1,3 +1,19 @@
+var values = [];
+values.push(0);
 export function getFizzBuzzUntil(n) {
-  // TODO: write your code here
+  for (var i=1; i <= n; i++)
+{   // if multiple of 15 replace index with FizzBuzz
+    if (i % 15 === 0)
+        values[i] = "FizzBuzz";
+  // if multiple of 3 replace index with Fizz
+    else if (i % 3 === 0)
+       values[i] = "Fizz";
+  //// if multiple of 5 replace index with Buzz
+    else if (i % 5 === 0)
+        values[i] = "Buzz";
+  //else input regular number into array 
+    else
+        values[i] = i;
+}
+return values;
 }
