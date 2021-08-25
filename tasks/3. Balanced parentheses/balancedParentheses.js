a00fbde5a4491141212adf010bfb1431c53c4638
omarshams97 <46492810+omarshams97@users.noreply.github.com>
2021-08-25 13:05:22 -0300

    Update balancedParentheses.js

--- tasks/3. Balanced parentheses/balancedParentheses.js
+++ tasks/3. Balanced parentheses/balancedParentheses.js
@@ -1,3 +1,28 @@
 export function areParenthesesBalanced(inputString) {
-  // TODO: write your code here
+  let stack = [];
+    let map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+    
+     for (let i = 0; i < inputString.length; i++) {
+
+        // If character is an opening brace add it to a stack
+        if (inputString[i] === '(' || inputString[i] === '{' || inputString[i] === '[' ) {
+            stack.push(inputString[i]);
+        }
+        //  If that character is a closing brace, pop from the stack, which will also reduce the length of the stack each time a closing bracket is encountered.
+        else {
+            let last = stack.pop();
+
+            //If the popped element from the stack, which is the last opening brace doesn’t match the corresponding closing brace in the map, then return false
+            if (inputString[i] !== map[last]) {return false};
+        }
+    }
+    // By the completion of the for loop after checking all the brackets of the inputString, at the end, if the stack is not empty then fail
+        if (stack.length !== 0) {return false};
+
+    return true;
 }
+
