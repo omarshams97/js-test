ab18ec3638f4e4e1d3c3ac304e91fcec20f7129e
omarshams97 <46492810+omarshams97@users.noreply.github.com>
2021-08-25 12:51:09 -0300

    Update fibonacci.js

--- tasks/2. Fibonacci - recursive/fibonacci.js
+++ tasks/2. Fibonacci - recursive/fibonacci.js
@@ -1,3 +1,17 @@
-export function getFibonacciUntil(n) {
-  //TODO write your code here, remember it should be recursive
+export function getFibonacciUntil(num) {
+//if num at 1 return 0,1 in an array 
+  if (num===1) 
+  {
+    return [0, 1];
+  } 
+  else 
+  {
+    //else make array equal to previous fib value  
+    var s =  getFibonacciUntil(num - 1);
+    //push approprate values into array and finally return array 
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
 }
+
+ //console.log( getFibonacciUntil(8));
