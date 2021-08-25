d6ba50ae665eb0f56752f231d9f10f9174e3e95c
omarshams97 <46492810+omarshams97@users.noreply.github.com>
2021-08-25 11:49:39 -0300

    Update palindrome.js
    Answer to Q1

--- tasks/1. Palindrome check/palindrome.js
+++ tasks/1. Palindrome check/palindrome.js
@@ -1,3 +1,10 @@
 export function isPalindrome(inputString) {
-  // TODO: write your code here
+  //RegExp to remove unwanted characters from input string 
+  var re = /[\W_]/g;
+  //turning all characters to lowercase and using RegExp to get rid of unwanted characters 
+  var lowRegStr = inputString.toLowerCase().replace(re, '');
+  //spliting the string into individual characters, reversing the character positions (first character becomes last, last becoms first ect) then recombining them into a string 
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+  //if the reverse string is equal to the intial it is a palindrome and return true, If not return false.
+  return reverseStr === lowRegStr;
 }
