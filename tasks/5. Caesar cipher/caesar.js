32632c2efd5a6e390282e19cc6a3089551ad908c
omarshams97 <46492810+omarshams97@users.noreply.github.com>
2021-08-25 13:37:03 -0300

    Update caesar.js

--- tasks/5. Caesar cipher/caesar.js
+++ tasks/5. Caesar cipher/caesar.js
@@ -1,3 +1,19 @@
+var MAX = 26;
 export function encryptCaesar(inputString, key) {
-  // TODO: write your code here
+ // Reduce x because rotation of
+    // length 26 is unnecessary
+    key = key % MAX;
+  //going through input string and decrementing letters by key
+  for(var i = 0; i < inputString.length; i++)
+    {
+       var pos = (inputString[i].charCodeAt(0) - 'a'.charCodeAt(0) - key);
+             
+            if (pos < 0)
+            {
+                pos += MAX;
+            }
+             
+            inputString[i] = String.fromCharCode(pos + 'a'.charCodeAt(0));
+    }
+  return inputString.join('');
 }
